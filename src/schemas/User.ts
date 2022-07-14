b29ad7c11f73d6861b9069ae64461f6f77708690
ricopomps <ricardopompiliodossantos@gmail.com>
2022-07-13 22:36:14 -0300

    refactor: Completed Schema and interface for user

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -2,11 +2,27 @@ import { Schema, model, Document } from 'mongoose';
 
 export interface UserInterface extends Document {
   name: string;
+  login: string;
+  password: string;
+  email: string;
+  phone: string;
+  cpf: string;
+  birthdate: string;
+  motherName: string;
+  status: string;
 }
 
 const UserSchema = new Schema(
   {
-    name: String
+    name: String,
+    login: String,
+    password: String,
+    email: String,
+    phone: String,
+    cpf: String,
+    birthdate: String,
+    motherName: String,
+    status: String
   },
   { timestamps: true }
 );
