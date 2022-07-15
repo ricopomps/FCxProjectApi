121f67c4089f05c75b3a402720ad600b12737012
ricopomps <ricardopompiliodossantos@gmail.com>
2022-07-15 00:03:06 -0300

    refactor: Adding basic error handling

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,32 +3,52 @@ import UserService from '../services/UserService';
 
 class UserController {
   public async index (req: Request, res: Response): Promise<Response> {
-    const users = await UserService.index();
-    return res.json(users);
+    try {
+      const users = await UserService.index();
+      return res.json(users);
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
   }
 
   public async findById (req: Request, res: Response): Promise<Response> {
-    const { id } = req.params;
-    const users = await UserService.findById(id);
-    return res.json(users);
+    try {
+      const { id } = req.params;
+      const users = await UserService.findById(id);
+      return res.json(users);
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
   }
 
   public async create (req: Request, res: Response): Promise<Response> {
-    const user = req.body;
-    const createdUser = await UserService.create(user);
-    return res.json(createdUser);
+    try {
+      const user = req.body;
+      const createdUser = await UserService.create(user);
+      return res.json(createdUser);
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
   }
 
   public async update (req: Request, res: Response): Promise<Response> {
-    const user = req.body;
-    const updatedUser = await UserService.update(user);
-    return res.json(updatedUser);
+    try {
+      const user = req.body;
+      const updatedUser = await UserService.update(user);
+      return res.json(updatedUser);
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
   }
 
   public async delete (req: Request, res: Response): Promise<Response> {
-    const user = req.body;
-    const deletedUser = await UserService.delete(user);
-    return res.json(deletedUser);
+    try {
+      const user = req.body;
+      const deletedUser = await UserService.delete(user);
+      return res.json(deletedUser);
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
   }
 }
 
